a43b44929374f4a8627b71eaff6785d3024780ac
LovishDutta <lovishdutta30@gmail.com>
2026-02-28 12:14:35 +0530

    div3

--- E_Divisive_Battle.cpp
+++ E_Divisive_Battle.cpp
@@ -1,60 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-
 signed main(){
 ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int tc=1;
     cin>>tc;    
     while(tc--){
-        int m,flag=1,flag2=0;
-        cin>>m;
-        vector<int> arr(m);
-        set<int> st;
-        for(int i=0;i<m;i++){
+        int n,flag=0,ans=0;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
             cin>>arr[i];
-            if(i>0&&arr[i]<arr[i-1]) flag2=1;
+            if(i>0&&arr[i]<arr[i-1]) flag=1;
         }
-        if(!flag2){
+        if(!flag){
             cout<<"Bob"<<endl;
             continue;
         }
-        for(int i=m-1;i>=0;i--){
+        set<int> st;
+        for(int i=n-1;i>=0;i--){
             if(arr[i]==1){
                 st.insert(1);
+                continue;
             }
-            else{
-                vector<int> ans;
-                int n=arr[i];
-                int cnt = 0;
-                for(int j = 2; j * j <= n; j++){
-                    if(n % j == 0){
-                    cnt++;
-                 while(n % j == 0){
-                n /= j;
-                ans.push_back(j);
-                if(!st.empty()&&j>*(st.begin())){
-                    flag=0;
-                    break;
+            vector<int> a;
+            for(int j=2;j*j<=arr[i];j++){
+                if(arr[i]%j==0){
+                    a.push_back(j);
+                    if(!st.empty()&&j>*(st.begin())){
+                        ans=1;
+                        break;
+                    }
+                    while(arr[i]%j==0){
+                        arr[i]/=j;
+                    }
+                }   
+            }
+                if(arr[i]>1){
+                    a.push_back(arr[i]);
+                    if(!st.empty()&&arr[i]>*(st.begin())){
+                        ans=1;
+                        break;
+                    }
                 }
-                }
-                }
-                }
-            if(n > 1){
-                ans.push_back(n);
-                cnt++;
-               if(!st.empty()&&n>*(st.begin())) flag=0;
-             }
-            if(flag==0||cnt>1){
-                flag=0;
+                if(ans==1||(a.size()>1)){
+                ans=1;
                 break;
             }
-            for(auto &it: ans){
-                st.insert(it);
-            }
-            }
+            for(auto &it:a) st.insert(it);
         }
-        if(flag) cout<<"Bob"<<endl;
-        else cout<<"Alice"<<endl;
+        if(ans) cout<<"Alice"<<endl;
+        else cout<<"Bob"<<endl;
     }
 }
